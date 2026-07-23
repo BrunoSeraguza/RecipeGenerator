a1653f19102a9f9d3a33be428296a569f0926a31
Bruno Seraguza <shzdevops@gmail.com>
2026-07-22 22:00:21 -0300

    feat: Crate register user endpoint

---
+++ src/Backend/MyRecipeBookGenerator.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBookGenerator.Communication.Request;
+
+namespace MyRecipeBookGenerator.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register(RequestRegisterUserAccountJson request)
+    {
+        return Created();       
+    }
+}
---
+++ src/Shared/MyRecipeBookGenerator.Communication/Request/RequestRegisterUserAccountJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBookGenerator.Communication.Request;
+
+public class RequestRegisterUserAccountJson
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
