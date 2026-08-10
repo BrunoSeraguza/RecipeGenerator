3aa366b1aedfb8abe126074d0c1d6aacdc153c25
Bruno Seraguza <shzdevops@gmail.com>
2026-08-10 20:54:52 -0300

    feat: adicionando exceptions personalizadas

--- src/Backend/MyRecipeBookGenerator.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
+++ src/Backend/MyRecipeBookGenerator.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using MyRecipeBookGenerator.Communication.Request;
+using MyRecipeBookGenerator.Exception.ExceptionsBase;
 
 namespace MyRecipeBookGenerator.Application.UseCases.User.Register;
 
@@ -9,6 +10,14 @@ public class RegisterUserAccountUseCase
         var validate = new RegisterUserAccountValidator();
 
         var result =  validate.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorsMessage = result.Errors.Select(erro => erro.ErrorMessage).ToList();
+
+            throw new ErrorOnValidatorException(errorsMessage);
+        }
+
 
     }
 }
---
+++ src/Shared/MyRecipeBookGenerator.Exception/ExceptionsBase/MyRecipeBookException.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBookGenerator.Exception.ExceptionsBase;
+
+//Não podemos criar uma instancia para uma calsse abstrada
+public abstract class MyRecipeBookException : System.Exception
+{
+
+}
