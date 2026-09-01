811d667602b0fb75273f68b673df4a5616b8f68d
Bruno Seraguza <shzdevops@gmail.com>
2026-09-01 19:39:34 -0300

    feat: Add user entitie

--- src/Backend/MyRecipeBookGenerator.Domain/Entities/User.cs
+++ src/Backend/MyRecipeBookGenerator.Domain/Entities/User.cs
@@ -2,7 +2,7 @@
 
 public class User
 {
-    public long Id { get; set; }
+    public Guid Id { get; private set; } =  Guid.NewGuid();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool Active { get; set; } = true;
     public string Name { get; set; } = string.Empty;
