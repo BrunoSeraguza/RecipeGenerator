3814d2c3e43cbcbc83935684d05692646a9087e3
Bruno Seraguza <shzdevops@gmail.com>
2026-09-10 19:49:07 -0300

    feat: adicionando hash com argon2

---
+++ src/Backend/MyRecipeBookGenerator.Domain/Security/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBookGenerator.Domain.Security.PasswordHasher;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
+}
